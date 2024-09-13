98217c165478538056478ad67e1e6a95e21af742
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-13 22:41:54 +0300

    Update and rename main.cpp to For.cpp

---
+++ For.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+
+	for (int i = 0; i < 10; i++) { //Цикл for это можно сказать тоже самое что и цикл while | A for loop is basically the same thing as a while loop
+		cout << "Переменная i = " << i << endl;
+	}
+
+
+}
